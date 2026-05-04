1a4c7721dd8cb5e18db3f418908660fa6005ea37
madhesh-maasi <madheshmaasi3@gmail.com>
2026-05-04 17:34:08 +0530

    Initial push

---
+++ src/webparts/transitBreakage/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITransitBreakageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TransitBreakageWebPartStrings' {
+  const strings: ITransitBreakageWebPartStrings;
+  export = strings;
+}
